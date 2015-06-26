75e1fe2f036cd2c15175eb2c86d9b04400f6b7f1
Rineroth <pp.bto92@gmail.com>
2015-06-26 14:46:45 +0000

    most recent update

--- BotCode.py
+++ BotCode.py
@@ -1,15 +1,3 @@
-#----------------------------------------------------------------------------------------
-#		Rineroth's GroupMe Bot
-#		Bot Functions:
-#			*Rate images that come from links that end in .jpg
-#			*Fix Reddit links. (Links must be the last thing written in the message)
-#			*ReactionGif Replacement
-#			*Bot agrees with you when asked "Right Bot?" 
-#			*Bot analyzes the message when it is mentioned in a message and then replies
-#			*Bot will read the first comment of a reddit thread when linked
-#			*Bot will adress each chat member properly by their name
-#			*Bot will react to specific calls such as "link?" and "you just got burned"
-#-----------------------------------------------------------------------------------------
 from flask import Flask, request
 import requests
 import json
@@ -17,43 +5,38 @@ import random
 import MySQLdb
 import re
 import praw
+import os
+import soundcloud
 
 #variable initialization
 app = Flask(__name__)
-bot_id = 'YOUR_BOT_ID_HERE'
-bot_name = 'YOUR_BOT_NAME'
+bot_id = '<YOUR BOT ID HERE>'
+bot_name = 'Robertobot'
 prevUser = ""
 count = 0
-r = praw.Reddit('REDDIT_USER_AGENT')
+r = praw.Reddit('RobBot_UserAgent')
 
 @app.route('/', methods=['POST'])
 def rob_callback():
 		global prevUser
 		global count
-		conn = MySQLdb.connect(host= "127.0.0.1",user="DB_USERNAME",passwd="db_PASSWORD",db="DB_NAME",charset='utf8')
+		conn = MySQLdb.connect(host= "127.0.0.1",user="<USER>",passwd="<PASSWORD>",db="<DATABASE NAME>",charset='utf8')
 		messageData = json.loads(request.data)
 		print (messageData['user_id'], messageData['name'])
-		if messageData['user_id'] == "USER ID NUMBER":
-			theUser = "USERNAME"
-		if messageData['user_id'] == "USER ID NUMBER":
-			theUser = "USERNAME"
-		if messageData['user_id'] == "USER ID NUMBER":
-			theUser = "USERNAME"
-		if messageData['user_id'] == "USER ID NUMBER":
-			theUser = "USERNAME"
-		#Keep adding statements for username. The reason why usernames are not dinamic is because the users in my chat change their names constantly
 
-
+		if messageData['user_id'] == "<USER ID>":
+			theUser = "<NAME>"
+		elif messageData['user_id'] == "<ANOTHER USER>":
+			theUser = "<NAME>"
 		else:
 			theUser = prevUser
 		if theUser == prevUser:
 			count += 1
 		else :
 			count = 0
-		#This statement stops the bot from replying to himself
+
 		if messageData['name'] != bot_name:
-			#statement to check for jpg links
-			if ".jpg" in messageData['text'].lower():
+			if ".jpg" in messageData['text'].lower() or len(messageData['attachments'])>0:
 				rating = random.randrange(1,10+1)
 				if rating > 7:
 					gmeSendback = ""
@@ -61,31 +44,25 @@ def rob_callback():
 				if rating <= 7:
 					gmeSendback = ""
 					gmeSendback = str(rating)+"/10 wouldn'tBang.exe(ERROR), bad post %s" % (theUser,)
-			#if a user talks too much, the bot will complain
 			elif count == 5:
-				gmeSendback = "Please stop talking with yourself %s" % (theUser,)	
-			#statement to check for gifs in the database			
+				gmeSendback = "Please stop talking with yourself %s" % (theUser,)				
 			elif ".gif" in messageData['text'].lower() and "/" not in messageData['text'].lower():
 				x = conn.cursor()
 				messageS = messageData['text'].lower()
 				checkList = messageS.split()
 				if len(checkList) == 1:
 					try:
-						x.execute("""SELECT COLUMN_NAME FROM TABLE_NAME WHERE ATTRIBUTE=%s""",(str(messageData['text'].lower()), ))
+						x.execute("""SELECT imgurLink FROM TheBIN WHERE callGif=%s""",(str(messageData['text'].lower()), ))
 						gmeSendback=x.fetchone()[0]
 					except:
-						gmeSendback = "Sorry %s, no gif found in the bin, add it on THE DATABASE" % (theUser,)
-			#reddit link fixer
+						gmeSendback = "Sorry %s, no gif found in the bin" % (theUser,)
 			elif "r/" in messageData['text'].lower() and "www." not in messageData['text'].lower() and ".com" not in messageData['text'].lower():
 				m= re.search('(?<=r/)\w+',messageData['text'].lower())
-				gmeSendback = "Here is your link %s! www.reddit.com/r/" % (theUser,)+m.group(0)
-			#you just got burned statement
+				gmeSendback = "Here is your link %s! http://www.reddit.com/r/" % (theUser,)+m.group(0)
 			elif "you just got burned." in messageData['text'].lower():
-				gmeSendback = "Get help with your burn! http://en.wikipedia.org/wiki/List_of_burn_centers_in_the_United_Stateshttp://en.wikipedia.org/wiki/List_of_burn_centers_in_the_United_States"
-			#link statement
+				gmeSendback = "Get help with your burn! http://en.wikipedia.org/wiki/List_of_burn_centers_in_the_United_States"
 			elif "link?" in messageData['text'].lower():
 				gmeSendback = "Here's Link, The Hero of Time: http://imgur.com/NNmOyg3.jpg"
-			#when the bot's name is mentioned in a message, it will analyse the message and reply based on the comment
 			elif bot_name.lower() in messageData['text'].lower():       
 				messageSentiment = requests.post('http://text-processing.com/api/sentiment/', ('text=' + str(messageData['text'])))
 				sLabel = messageSentiment.json()['label']
@@ -96,17 +73,30 @@ def rob_callback():
 					gmeSendback = "Your response was meh %s ! Try harder!" % (theUser,)
 				else:
 					gmeSendback = "U wot %s? il rek u cheeky cunt" % (theUser,)
-			#the bot will agree with you
+
+				
 			elif "right bot?" in messageData['text'].lower():       
 				gmeSendback = "You are correct %s" %(theUser,)
-			#the bot will read the first comment of a reddit thread
+
+			elif "bot, say:" in messageData['text']:
+				text=messageData['text']
+				editText=text[text.rfind(':')+2:]
+				editText=re.sub(r'[^\w]', ' ', editText)
+				textoo = editText
+				os.system("espeak \" %s \"  -w \"%s.wav\" -s 140"%(editText,textoo))
+				 # create client object with app credentials
+				client = soundcloud.Client(client_id='<SOUNDCLOUD ID>',client_secret='<SOUNDCLOUD PASS>',username='<SOUNDCLOUD USERNAME>',password='<SOUNDCLOUD PASS>')
+				# upload audio file
+				track = client.post('/tracks', track={'title': '%s'%(editText,),'asset_data': open('%s.wav'%(editText,), 'rb')})
+				# print track link
+				gmeSendback = track.permalink_url
+
 			elif "reddit.com" in messageData['text'].lower()and"comments"in messageData['text'].lower():
 				submission = r.get_submission(messageData['text']).comments[0]
 				if len(submission.body) > 430:
 					gmeSendback = "TL;TR (Too lazy to read that long ass comment)"
 				else:
 					gmeSendback = submission.body
-			#the bot will reply if he's up and running
 			elif "are you alive bot?" in messageData['text'].lower():
 				gmeSendback = "yes master, I am fully functional"
 			else:
@@ -125,3 +115,5 @@ if __name__ == "__main__":
         main()
         app.debug = True
         app.run('0.0.0.0', 8081)
+
+ 
